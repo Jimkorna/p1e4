b61caddc0466bbcdbeeb546836ccc14b71ec27ac
Jimkorna <72735841+Jimkorna@users.noreply.github.com>
2020-11-18 17:36:17 +0200

    Add files via upload

---
+++ complexcalculations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+float c;
+int d;
+float pro,af,pol,di,up;
+printf("Dwse enan dekadiko arithmo:  ");
+scanf("%f",&c);
+printf("Dwse enan akereo arithmo:  ");
+scanf("%d", &d);
+pro=c+d;
+af=c-d;
+pol=c*d;
+di=c/d;
+up=(int)c % d;
+printf("To apotelesma tis prosthshs(+) twan arithmwn einai %f\n",pro);
+printf("To apotelesma tis afairesis(-) twan arithmwn einai %f\n", af);
+printf("To apotelesma tou poll/mou(*) twan arithmwn einai %f\n", pol);
+printf("To apotelesma tis dieraisis(/) twan arithmwn einai %f\n", di);
+printf("To akeraio upoloipo twn aritmwn einai: %f", up);
+}
